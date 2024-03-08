e68acec55cb6b3ef96a55163fbaa8121c51a3a40
ParisaMousavi <ParisaMousavi>
2024-03-08 16:57:08 +0000

    update

--- decision-tree/select-platform-prediction.py
+++ decision-tree/select-platform-prediction.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 # save filepath to variable for easier access
-melbourne_file_path = '/Workspace/Repos/parisamoosavinezhad@hotmail.com/ai-samples/kaggle-lesson-1/input/melb_data.csv'
+melbourne_file_path = '/Workspace/Repos/parisamoosavinezhad@hotmail.com/ai-samples/decision-tree/input/decision-tree-scenarios-samples.csv'
 # read the data and store data in DataFrame titled melbourne_data
 melbourne_data = pd.read_csv(melbourne_file_path) 
 # print a summary of the data in Melbourne data
