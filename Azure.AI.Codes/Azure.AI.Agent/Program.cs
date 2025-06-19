e3546a7fac1747c3467a14151fd7dec1acbbae0f
Parisa Moosavinezhad <parisa.moosavinezhad@devoteam.com>
2025-06-19 22:51:29 +0200

    added link

--- Azure.AI.Codes/Azure.AI.Agent/Program.cs
+++ Azure.AI.Codes/Azure.AI.Agent/Program.cs
@@ -4,6 +4,8 @@ using Azure.Identity;
 using Microsoft.Extensions.Configuration;
 using System.Diagnostics;
 
+// Page: https://learn.microsoft.com/en-us/azure/ai-foundry/agents/quickstart?pivots=programming-language-csharp
+
 IConfigurationRoot configuration = new ConfigurationBuilder()
     .SetBasePath(AppContext.BaseDirectory)
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
