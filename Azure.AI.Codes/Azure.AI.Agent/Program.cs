3f8d432da3ffde3b9aea76fc64b3e81c9fc52c15
Parisa Moosavinezhad <parisa.moosavinezhad@devoteam.com>
2025-06-19 22:47:59 +0200

    updated

---
+++ Azure.AI.Codes/Azure.AI.Agent/Program.cs
@@ -0,0 +1,14 @@
+﻿using Azure;
+using Azure.AI.Agents.Persistent;
+using Azure.Identity;
+using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+
+IConfigurationRoot configuration = new ConfigurationBuilder()
+    .SetBasePath(AppContext.BaseDirectory)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+// See https://aka.ms/new-console-template for more information
+Console.ReadLine();
+Console.WriteLine("Hello, World!");
